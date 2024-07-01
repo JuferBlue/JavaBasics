0760e2dc98572f8020d3ecb1e39984892bec8a32
JuferBlue <2846823009@qq.com>
2024-07-01 23:10:58 +0800

    feat(functions): add method definitions and调用 in Function_09.java
    This commit introduces new methods `my_print` and `getMax` in the `Function_09.java`file. It also includes main method updates to demonstrate the usage of these
    new methods. Additionally, the commit adds comments for method descriptions
    and demonstrates method overloading with different `add` method signatures.

--- SyntaxBasics/src/main/java/Function_09.java
+++ SyntaxBasics/src/main/java/Function_09.java
@@ -7,5 +7,49 @@
  * @Version: 1.0
  */
 public class Function_09 {
+    public static void main(String[] args) {
+        //无参数方法的定义和调用
+        my_print();
+        //带参数方法的定义和调用
+        System.out.println(getMax(10,20));
+    }
+
+    /**
+     * @description: 1-无参数方法的定义和调用
+     * @author: JuferBlue
+     * @date: 2024/7/1 22:35
+     * @param:
+     * @return: void
+     **/
+    public static void my_print(){
+        System.out.println("hello function");;
+    }
+
+    /**
+     * @description:2-带参数的方法
+     * @author: JuferBlue
+     * @date: 2024/7/1 22:36
+     * @param: a
+     * @param: b
+     * @return: int
+     **/
+    public static int getMax(int a,int b){
+        return a>b?a:b;
+    }
+
+    //3-方法重载:
+    //多个方法在同一个类中
+    //多个方法具有相同的方法名
+    //多个方法的参数不相同，类型不同或者数量不同
+    public static int add(int a,int b){
+        System.out.println("我是int的add方法");
+        return a+b;
+    }
+    public static double add(double a,double b){
+        System.out.println("我是double的add方法");
+        return a+b;
+    }
+
+
 
 }
