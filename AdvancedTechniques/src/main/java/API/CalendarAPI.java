409a373808b937e228e9f0317c732c021f183c81
JuferBlue <2846823009@qq.com>
2024-07-26 00:17:20 +0800

    Date

---
+++ AdvancedTechniques/src/main/java/API/CalendarAPI.java
@@ -0,0 +1,17 @@
+package API;
+
+/**
+ * @BelongsProject: JavaBasics
+ * @BelongsPackage: API
+ * @Author: JuferBlue
+ * @CreateTime: 2024-07-26  00:12
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class CalendarAPI {
+    public static void main(String[] args) {
+        //Calendar代表了当前系统日历对象，可以单独修改获取时间中的年月日
+        //是抽象类，不能直接实例化，需要使用子类对象
+
+    }
+}
--- AdvancedTechniques/src/main/java/API/SimpleDataFormatAPI.java
+++ AdvancedTechniques/src/main/java/API/SimpleDataFormatAPI.java
@@ -1,5 +1,9 @@
 package API;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * @BelongsProject: JavaBasics
  * @BelongsPackage: API
@@ -9,8 +13,24 @@ package API;
  * @Version: 1.0
  */
 public class SimpleDataFormatAPI {
-    public static void main(String[] args) {
-        // 1. 获取SimpleDateFormat对象
-        // 2. 调用方法
+    public static void main(String[] args) throws ParseException {
+        Date d1 = new Date();
+        System.out.println(d1);
+
+        //1.创建SimpleDateFormat对象
+        SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy年MM月dd日 HH时mm分ss秒");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); //两种常用格式
+
+        //2.日期对象转换为字符串
+        String str1 = sdf1.format(d1);
+        String str2 = sdf2.format(d1);
+        System.out.println(str1);
+        System.out.println(str2);
+
+        //3.字符串转换为日期对象
+        Date d2 = sdf1.parse(str1);
+        Date d3 = sdf2.parse(str2);
+        System.out.println(d2);
+        System.out.println(d3);
     }
 }
